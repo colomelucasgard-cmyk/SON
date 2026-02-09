7c5271cf3ebbbc514402ab82e586a2d8bd9deb67
colomelucasgard-cmyk <colomelucas.gard@gmail.com>
2026-02-09 16:51:07 +0100

    Update BitCrusherEffect.ino

--- BitCrusherEffect/BitCrusherEffect.ino
+++ BitCrusherEffect/BitCrusherEffect.ino
@@ -1,39 +1,72 @@
 #include <Audio.h>
-#include "BitCrusherEffect.h"
+#include "BitCrusherEffectv2.h" // Ton effet Faust
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
 
-AudioSynthWaveform sine1; 
+AudioInputUSB      usb1;           
 
-BitCrusherEffect mycrusher;
-AudioOutputI2S out;
+BitCrusherEffectv2 mycrusher;      // Ton effet
+AudioOutputI2S     out;            // Sortie Casque (Shield)
 AudioControlSGTL5000 audioShield;
 
-AudioConnection patchCordInput(sine1, 0, mycrusher, 0);
-AudioConnection patchCord0(mycrusher, 0, out, 0);
-AudioConnection patchCord1(mycrusher, 0, out, 1);
+
+AudioConnection patchCord1(usb1, 0, mycrusher, 0);
+
+AudioConnection patchCord2(mycrusher, 0, out, 0);
+AudioConnection patchCord3(mycrusher, 0, out, 1);
+
+const int PIN_BOUTON = 3;    
+int etatBoutonPrecedent = HIGH; 
+bool filtreActif = false;
 
 void setup() {
-  AudioMemory(12); 
-  audioShield.enable();
-  audioShield.volume(0.3);
+  // Allouer assez de mémoire pour le tampon USB
+  Serial.begin(9600);
+  AudioMemory(20); 
   
-  // SAWTOOTH et une fréquence plus basse (100Hz)
-  sine1.begin(0.4, 100, WAVEFORM_SAWTOOTH); 
+  audioShield.enable();
+  audioShield.volume(0.5); 
+  pinMode(PIN_BOUTON, INPUT);
+  
+  
+ 
 }
 
 void loop() {
+  // --- GESTION DES POTENTIOMÈTRES (Inchangé) ---
+  
   int lecturepot1 = analogRead(A0);
-
-  float bitsvalue = (float)map(lecturepot1, 0, 1023, 16, 1); 
+  float bitsvalue = (float)map(lecturepot1, 0, 1023, 16, 2); 
   
   int lecturepot2 = analogRead(A2); 
   float Dsvalue = (float)map(lecturepot2, 0, 1023, 1, 50);
-  
+
+  int lectureBouton = digitalRead(PIN_BOUTON);
+  //Serial.println(lectureBouton); 
+
   mycrusher.setParamValue("Bits", bitsvalue);       
   mycrusher.setParamValue("Downsample", Dsvalue);
-  
+
+  if (lectureBouton == LOW && etatBoutonPrecedent == HIGH) {
+    // 1. On inverse l'état du filtre
+    filtreActif = !filtreActif; 
+    
+    // 2. On envoie la commande à Faust (0 ou 1)
+    if (filtreActif) {
+      mycrusher.setParamValue("AntiAlias", 1.0);
+      Serial.println("Filtre: ON"); // Utile pour vérifier
+    } else {
+      mycrusher.setParamValue("AntiAlias", 0.0);
+      Serial.println("Filtre: OFF");
+    }
   delay(50);
+  }
+  
+  etatBoutonPrecedent = lectureBouton;
+  
+  
+  
+  delay(20);
 }
